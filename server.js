ebf9ee2c486c6229be0d22d6fa663bf4887c19df
Ali-10011 <98711622+Ali-10011@users.noreply.github.com>
2022-09-10 02:38:50 +0500

    Properly Setup Auth req Service with dependencies

--- server.js
+++ server.js
@@ -13,83 +13,66 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.post('/',(req, res)=>{//requested url
-    //response.send('<p>Hello, its my homepage</p>'); //we 'send' the response
-    //console.log(JSON.parse(request));
-
-    console.log(req.body.username);
-    console.log(req.body.password);
-    console.log(req.body.authtype);
-    
-    if(req.body.authtype == 'signup')
+    if(req.body.authtype == "signup")
     {
-    const newAuth = new Auth({username: req.body.username, password:req.body.password }); //'Blog' is the name of  our  model th at we created  //The request body is sent using the 'names' that we defined
-    newAuth.save().then((result)=>{ const responseData = {
-        message:"signup OK"
-    }
-     
-    const jsonContent = JSON.stringify(responseData);
-    res.end(jsonContent);
-
-}).catch((err)=>{console.log(err);
-
-});
+       const username = req.body.username.toString();
     
-    }
-    else 
-    {
-Auth.findById(req.username).then((data)=>{  
-   const responseData = {
-    message:"Login OK"
+       Auth.findOne({'username' : username }, function (err, obj)
+       {
+        if (err) return handleError(err);
+        
+        if (!(obj)) //null object means that the user does not exist
+        {
+            const newAuth = new Auth({username: req.body.username, password:req.body.password }); 
+            newAuth.save().then((result)=>{ const responseData = {
+                message:"OK"
+            }
+            const jsonContent = JSON.stringify(responseData);
+            res.end(jsonContent);
+        
+        }).catch((err)=>{
+        console.log(err);     
+        }
+        );
+       }
+       else 
+       {
+        const responseData = {
+            message:"User Already Exists!"
+        }
+        const jsonContent = JSON.stringify(responseData);
+            res.end(jsonContent);
+       }
+    });
 }
- 
-const jsonContent = JSON.stringify(responseData);
-res.end(jsonContent);  }).catch((err)=>{console.log(err)});
+    else if (req.body.authtype == 'login')
+    {
+        const username = req.body.username.toString();
+        var _responsemessage;
+        Auth.findOne({'username' : username }, function (err, obj)
+        {
+         if (err) return handleError(err);
+         if (!(obj))
+         {
+            _responsemessage = "This User Does not Exist!";
+         }
+         else if(obj.password.toString() == req.body.password.toString())
+         {
+            _responsemessage = "OK";
+         }
+         else if (obj.password != req.body.password)
+         {
+            _responsemessage = "Wrong Password!";
+           
+         }
+         responseData = {
+            message:  _responsemessage
+        }
+         const jsonContent = JSON.stringify(responseData);
+        res.end(jsonContent);
+        });
+       
+  
     }
    
 });
-
-// const express = require('express');
-// const { Server } = require('ws');
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCU44eqvrt_LO2Sk2wuB7Zhe0cGLoLWWJE",
-//   authDomain: "project-note-bca4a.firebaseapp.com",
-//   projectId: "project-note-bca4a",
-//   storageBucket: "project-note-bca4a.appspot.com",
-//   messagingSenderId: "24249137468",
-//   appId: "1:24249137468:web:fd017a6ca15c1fee0a4639",
-//   measurementId: "G-6ZXW4SN246"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// let database = firebase.database();
-
-
-// const server = express()
-//     .use((req, res) => res.send("Hi there"))
-//     .listen(3000, () => console.log(`Listening on 3000`));
-
-// const wss = new Server({ server });
-
-// wss.on('connection', function(ws, req) {
-//     ws.on('message', message => {
-//         var dataString = message.toString();
-//         if (dataString == "Hello") {
-//             console.log(dataString)
-//             ws.send("Hi from Node.js");
-//         } else{
-//             console.log(dataString)
-//             ws.send("Are you not saying hi to me 🥺👉👈");
-//         }
-//     }) 
-// })
-
