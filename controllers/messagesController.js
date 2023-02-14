2c4b34dd9bfbfd4f797d6eef78bde7f07ede742e
Ali-10011 <ali.hussnainbabar@gmail.com>
2023-02-15 00:06:25 +0500

    Added Internal server error catch blocks

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -7,69 +7,62 @@ const Auth = require("../models/auth");
 const UserMessageInstance = require("../models/userdata");
 const querystring = require("querystring");
 
-
 //getting messages from server
-const getMessages = async (req, res) =>
-{
-    const queryObject = url.parse(req.url, true).query;
-    console.log(queryObject);
-    var messagesToLoad = parseInt(queryObject.perpage);
-    var skipMessages = parseInt(queryObject.skip);
-    UserMessageInstance.find({ username: "Lucifer" })
-      .limit(messagesToLoad)
-      .skip(skipMessages)
-      .sort({ createdAt: -1 })
-      .then((result) => {
-        const jsonContent = JSON.stringify(result);
-        res.end(jsonContent);
-      });
-}
+const getMessages = async (req, res) => {
+  const queryObject = url.parse(req.url, true).query;
+  try {
+    const messages = await UserMessageInstance.find({ username: "Lucifer" })
+      .limit(parseInt(queryObject.perpage))
+      .skip(parseInt(queryObject.skip))
+      .sort({ createdAt: -1 });
+
+    res.status(200).json(messages);
+  } catch (e) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 //posting a single message
-const uploadMessage = async(req, res) =>
-{
-    console.log(req.body);
+const uploadMessage = async (req, res) => {
+  try {
+    const message = await new UserMessageInstance({
+      username: "Lucifer",
+      message: req.body.message,
+      path: req.body.path,
+      mediatype: req.body.mediatype,
+      dateTime: req.body.dateTime,
+      isUploaded: "true",
+    }).save();
 
-  const newInstance = new UserMessageInstance({
-    username: "Lucifer",
-    message: req.body.message,
-    path: req.body.path,
-    mediatype: req.body.mediatype,
-    dateTime: req.body.dateTime,
-    isUploaded: "true",
-  });
-  newInstance
-    .save()
-    .then((result) => {
-      const responseData = {
-        message: "Message Stored",
-        result,
-      };
-      console.log(result);
-      const jsonContent = JSON.stringify(responseData);
-      res.end(jsonContent);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
+    if (!message) {
+      res.status(404).json({ error: "No such entry" });
+    }
+
+    res.status(200).json(messagee);
+  } catch (e) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 //deleting a message
-const deleteMessage = async (req, res) =>
-{
-    console.log(req.params.id)
+const deleteMessage = async (req, res) => {
+  try {
+    const message = await UserMessageInstance.findOneAndDelete({
+      _id: req.params.id,
+    });
 
-    UserMessageInstance.find({ _id: req.params.id })
-      .remove()
-      .then((result) => {
-        const jsonContent = JSON.stringify(result);
-        res.end(jsonContent);
-      });
-}
+    if (!message) {
+      res.status(404).json({ error: "No such entry" });
+    }
 
+    res.status(200).json(message);
+  } catch (e) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 module.exports = {
-    getMessages,
-    uploadMessage,
-    deleteMessage
-}
+  getMessages,
+  uploadMessage,
+  deleteMessage,
+};
